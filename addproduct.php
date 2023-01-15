2b571279f3e633a93629824d0eb85e3e0643ee20
Nicola Cecchetto <nicola.cecchetto.01@gmail.com>
2023-01-15 11:13:19 +0100

    completata validazione php e javascript

--- addproduct.php
+++ addproduct.php
@@ -3,8 +3,8 @@
 // CONNESSIONE AL DB
 
 $HTMLpage = file_get_contents("addproduct.html");
-$allowedTags = '<span>';
-$errors = "";
+$messaggi = "";
+$model = "";
 $brand = "";
 $color = "";
 $price = "";
@@ -14,12 +14,12 @@ $frets = "";
 $body = "";
 $fretboard = "";
 $pickupConf = "";
-$pickupType = "";
+$pickupType = "-";
 $description = "";
 
 function cleanInput($value) {
     $value = trim($value);
-    $value = strip_tags($value, $allowedTag);
+    $value = strip_tags($value, '<span>');
     return $value;
 }
 
@@ -31,102 +31,98 @@ function checkProhibitNumbers($value) {
     return preg_match("/^[a-zA-Z\-\ ]+$/", $value);
 }
 
-function checkOnlyNumbers($value) {
-    return preg_match("/^[0-9\.]+$/", $value);
-}
-
 if(isset($_POST['formSubmit'])) { // if user clicked submit button do this
     $brand = cleanInput($_POST['formBrand']);
     if(strlen($brand) == 0) {
-        $errors .= "<li>Marchio non può essere vuoto!</li>";
+        $messaggi .= "<li>Marchio non può essere vuoto!</li>";
     } else {
         $brandNoTags = strip_tags($brand);
         if(!checkAllowNumbers($brandNoTags)) {
-            $errors .= "<li>Il marchio può contenere solo lettere, numeri e trattini -!</li>";
+            $messaggi .= "<li>Il marchio può contenere solo lettere, numeri, spazi e trattini!</li>";
         }
     }
     $model = cleanInput($_POST['formModel']);
     if(strlen($model) == 0) {
-        $errors .= "<li>Modello non può essere vuoto!</li>";
+        $messaggi .= "<li>Modello non può essere vuoto!</li>";
     } else {
         $modelNoTags = strip_tags($model);
         if(!checkAllowNumbers($modelNoTags)) {
-            $errors .= "<li>Il modello può contenere solo lettere, numeri e trattini -!</li>";
+            $messaggi .= "<li>Il modello può contenere solo lettere, numeri, spazi e trattini!</li>";
         }
     }
     $color = cleanInput($_POST['formColor']);
     if(strlen($color) == 0) {
-        $errors .= "<li>Finitura non può essere vuoto!</li>";
+        $messaggi .= "<li>Finitura non può essere vuoto!</li>";
     } else {
         $colorNoTags = strip_tags($color);
         if(!checkProhibitNumbers($colorNoTags)) {
-            $errors .= "<li>La finitura può contenere solo lettere e trattini -!</li>";
+            $messaggi .= "<li>La finitura può contenere solo lettere, spazi e trattini!</li>";
         }
     }
     $price = cleanInput($_POST['formPrice']);
     if(strlen($price) == 0) {
-        $errors .= "<li>Prezzo non può essere vuoto!</li>";
+        $messaggi .= "<li>Prezzo non può essere vuoto!</li>";
     } else {
-        if(!checkOnlyNumbers($price)) {
-            $errors .= "<li>Il prezzo può contenere solo numeri!</li>";
+        if($price <= 0) {
+            $messaggi .= "<li>Inserire un prezzo maggiore di 0!</li>";
         }
     }
     $strings = cleanInput($_POST['formStrings']);
     if(strlen($strings) == 0) {
-        $errors .= "<li>Corde non può essere vuoto!</li>";
+        $messaggi .= "<li>Corde non può essere vuoto!</li>";
     } else {
-        if(!checkOnlyNumbers($strings)) {
-            $errors .= "<li>Corde può contenere solo numeri!</li>";
+        if($strings < 4 || $strings > 12) {
+            $messaggi .= "<li>Inserire un numero di corde tra 4 e 12!</li>";
         }
     }
     $frets = cleanInput($_POST['formFrets']);
     if(strlen($frets) == 0) {
-        $errors .= "<li>Tasti non può essere vuoto!</li>";
+        $messaggi .= "<li>Tasti non può essere vuoto!</li>";
     } else {
-        if(!checkOnlyNumbers($frets)) {
-            $errors .= "<li>Tasti può contenere solo numeri!</li>";
+        if($frets < 12 || $frets > 36) {
+            $messaggi .= "<li>Inserire un numero di tasti tra 12 e 36!</li>";
         }
     }
     $body = cleanInput($_POST['formBody']);
     if(strlen($body) == 0) {
-        $errors .= "<li>Legno del corpo non può essere vuoto!</li>";
+        $messaggi .= "<li>Legno del corpo non può essere vuoto!</li>";
     } else {
         $bodyNoTags = strip_tags($body);
         if(!checkProhibitNumbers($bodyNoTags)) {
-            $errors .= "<li>Legno del corpo può contenere solo lettere e trattini -!</li>";
+            $messaggi .= "<li>Legno del corpo può contenere solo lettere, spazi e trattini!</li>";
         }
     }
     $fretboard = cleanInput($_POST['formFretboard']);
     if(strlen($fretboard) == 0) {
-        $errors .= "<li>Legno della tastiera non può essere vuoto!</li>";
+        $messaggi .= "<li>Legno della tastiera non può essere vuoto!</li>";
     } else {
         $fretboardNoTags = strip_tags($fretboard);
         if(!checkProhibitNumbers($fretboardNoTags)) {
-            $errors .= "<li>Legno della tastiera può contenere solo lettere e trattini -!</li>";
+            $messaggi .= "<li>Legno della tastiera può contenere solo lettere, spazi e trattini!</li>";
         }
     }
     $pickupType = cleanInput($_POST['formPickupType']);
     if(strlen($pickupType) == 0) {
-        $errors .= "<li>Tipologia pickup non può essere vuoto!</li>";
+        $messaggi .= "<li>Tipologia pickup non può essere vuoto!</li>";
     } else {
         $pickupTypeNoTags = strip_tags($pickupType);
         if(!checkAllowNumbers($pickupTypeNoTags)) {
-            $errors .= "<li>Tipologia pickup può contenere solo lettere, numeri e trattini -!</li>";
+            $messaggi .= "<li>Tipologia pickup può contenere solo lettere, numeri, spazi e trattini!</li>";
         }
     }
     $description = cleanInput($_POST['formDescription']);
     if(strlen($description) == 0) {
-        $errors .= "<li>Descrizione non può essere vuoto!</li>";
+        $messaggi .= "<li>Descrizione non può essere vuoto!</li>";
     }
 
-    if($errors == "") {
-        // INSERT con tutti i casi
+    if($messaggi == "") {
+        $messaggi = "<p id='formSuccess'>Chitarra inserita con successo!</p>";
     } else {
-        $errors = "<div><ul>" . $errors . "</ul></div>";
+        $messaggi = "<ul class='formError'>" . $messaggi . "</ul>";
     }
 }
 
-$HTMLpage = str_replace('<messaggiForm />', $errors, $HTMLpage);
+$HTMLpage = str_replace('<messaggiForm />', $messaggi, $HTMLpage);
 $HTMLpage = str_replace('<valBrand />', $brand, $HTMLpage);
 $HTMLpage = str_replace('<valModel />', $model, $HTMLpage);
 $HTMLpage = str_replace('<valColor />', $color, $HTMLpage);
