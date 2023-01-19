b6a268756e6f5f51c09ebc993b89cd044f17e239
Niccolò Fasolo <niccolofasolo@gmail.com>
2023-01-19 10:51:44 +0100

    updated php files

--- prodotti.php
+++ prodotti.php
@@ -12,7 +12,7 @@ $listText = '';
 
 $newGuitarLink = '<a id="addNewGuitar" href="">Aggiungi chitarra</a>'; // link pagina nuova chitarra
 if (isset($_SESSION['session_id'])) {
-    $loggedInBtns = '<li><a href="./preferiti.php"><img src="./images/favourites.svg" height="44" width="44" alt="preferiti"/></a></li><li><a href="./utente.php"><img src="./images/account.svg" height="44" width="44" alt="account"/></a></li>';
+    $loggedInBtns = '<li><a href="./preferiti.php"><img src="./images/favourites.svg" height="44" width="44" alt="preferiti"/></a></li><li><a href="./utente.php"><img src="./images/account.svg" height="44" width="44" alt="area personale"/></a></li>';
     $HTMLpage = str_replace($loginBtns, $loggedInBtns, $HTMLpage);
     if ($_SESSION['role'] == 'admin') {
         $HTMLpage = str_replace('<linkNuovaChitarra />', $newGuitarLink, $HTMLpage);
